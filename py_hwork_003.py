9943d799430480866e0c0a8093b8af39455adc15
IntentG <sublustrum123@gmail.com>
2022-07-13 20:49:43 +0600

    Задача 3. Сумма цифр числа

---
+++ py_hwork_003.py
@@ -0,0 +1,20 @@
+'''Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+Пример:
+- 6782 -> 23
+- 0,56 -> 11'''
+
+number = input("Введите число, узнаем сумму его цифр:   ")
+
+def CalculateNumbers (number):
+    summ = 0
+    list = []
+    i = 0
+    for char in number:
+        if char == "." or char == ",":
+            char = 0
+        list.append(int(char))
+        summ = summ + list[i]
+        i += 1
+    return summ
+
+print("\nСумма всех цифр введённого числа: [", CalculateNumbers(number), "]")
